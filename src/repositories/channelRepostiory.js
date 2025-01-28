1fe6644337b76464cc4d6c659bde43481ff6c561
Jatin Bishnoi <jatinkumar160902@gmail.com>
2025-01-28 12:03:11 +0530

    added workspace and channel repo

---
+++ src/repositories/channelRepostiory.js
@@ -0,0 +1,7 @@
+import Channel from '../schema/channel.js';
+import crudRepository from './crudRepository.js';
+
+const channelRepository = {
+  ...crudRepository(Channel)
+};
+export default channelRepository
