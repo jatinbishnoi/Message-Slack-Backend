530f181a9c8a4fbdc8e7cb10360fd8a5c3ce5c3a
Jatin Bishnoi <jatinkumar160902@gmail.com>
2025-01-29 22:34:21 +0530

    completed workspaces

--- src/schema/channel.js
+++ src/schema/channel.js
@@ -1,4 +1,3 @@
-
 import mongoose from 'mongoose';
 
 const channelSchema = new mongoose.Schema(
@@ -6,8 +5,14 @@ const channelSchema = new mongoose.Schema(
     name: {
       type: String,
       required: [true, 'Channel name is required']
+    },
+    workspaceId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Workspace',
+      required: [true, 'Workspace ID is required']
     }
   },
+
   { timestamps: true }
 );
 
