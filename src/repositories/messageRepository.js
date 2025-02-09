dbf159ab5050b578c606801599bf6cf8827fc1f8
Jatin Bishnoi <jatinkumar160902@gmail.com>
2025-02-09 22:36:09 +0530

    Added new API

--- src/repositories/messageRepository.js
+++ src/repositories/messageRepository.js
@@ -1,4 +1,3 @@
-
 import Message from '../schema/message.js';
 import crudRepository from './crudRepository.js';
 
@@ -6,12 +5,19 @@ const messageRepository = {
   ...crudRepository(Message),
   getPaginatedMessaged: async (messageParams, page, limit) => {
     const messages = await Message.find(messageParams)
-      .sort({ createdAt: -1 })
+      .sort({ createdAt: 1 })
       .skip((page - 1) * limit)
       .limit(limit)
       .populate('senderId', 'username email avatar');
 
     return messages;
+  },
+  getMessageDetails: async (messageId) => {
+    const message = await Message.findById(messageId).populate(
+      'senderId',
+      'username email avatar'
+    );
+    return message;
   }
 };
 
